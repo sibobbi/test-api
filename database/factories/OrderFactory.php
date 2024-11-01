14a5bdd83dcbc090d627255c5fe0bd4db3860388
Vladislav <sibobbi101@gmail.com>
2024-11-01 18:01:41 +0700

    Fix factory

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Order;
 use App\Models\Product;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class OrderFactory extends Factory
@@ -12,10 +13,19 @@ class OrderFactory extends Factory
 
     public function definition()
     {
+        $randomDays = $this->faker->numberBetween(1, 10);
+        $date = $this->faker->dateTimeBetween('-0 month', 'now');
+
+// Преобразование в объект Carbon
+        $carbonDate = Carbon::instance($date);
+
+// Прибавление одного дня
+        $carbonDate->addDays($randomDays);
+
         return [
             'product_id' => Product::inRandomOrder()->first()->id,
             'count' => $this->faker->numberBetween(1, 10),
-            'created_at' => $this->faker->dateTimeBetween('-1 month', 'now'),
+            'created_at' => $carbonDate,
         ];
     }
 }
